98b936140c3b2bcd4edda959a9963778d13ed6be
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2016-02-02 17:58:17 +0100

    fixed indentation in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,12 @@ module.exports = function(grunt) {
                 src: 'lib/vjs-quality-picker.js',
                 dest: 'debug/vjs-quality-picker.js',
                 options:  {
-                   transform: ['babelify'],
-                   browserifyOptions: {
-                       debug: true
-                   },
-                   watch: true,
-                   keepAlive: true
+                  transform: ['babelify'],
+                  browserifyOptions: {
+                      debug: true
+                  },
+                  watch: true,
+                  keepAlive: true
                }
             }
         }
